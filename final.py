d9ace82b2163f6667ebd2e1c798cf4c0507f0ee9
Johnathan Dwayne Heath <jheath@student.wethinkcode.co.za>
2019-01-17 02:38:41 +0200

    90% done just need to fix ranks

--- final.py
+++ final.py
@@ -14,29 +14,33 @@ for line in f:
 
     team_1_score = team_1_exploded.pop(len(team_1_exploded) - 1)
     team_1_name = ' '.join(team_1_exploded).strip()
-    # print(team_1_name + ': ' + team_1_score)
 
     team_2_score = team_2_exploded.pop(len(team_2_exploded) - 1)
     team_2_name = ' '.join(team_2_exploded).strip()
-    # print(team_2_name + ': ' + team_2_score)
 
     if (team_scores.get(team_1_name) == None):
         team_scores[team_1_name] = 0
-
     if (team_scores.get(team_2_name) == None):
         team_scores[team_2_name] = 0
     # assigning points to teams based on win/lose/draw
-    if (team_1_score == team_2_score):
-        team_scores[team_1_name] = team_scores[team_1_name] + 1
-        team_scores[team_1_name] = team_scores[team_2_name] + 1
-    elif(team_1_score > team_2_score):
-        team_scores[team_1_name] = team_scores[team_1_name] + 3
-    elif(team_2_score > team_1_score):
-        team_scores[team_2_name] = team_scores[team_2_name] + 3
-
+    if (int(team_1_score) == int(team_2_score)):
+        team_scores[team_1_name] += 1
+        team_scores[team_2_name] += 1
+    if(int(team_1_score) > int(team_2_score)):
+        team_scores[team_1_name] += 3
+        team_scores[team_2_name] += 0
+    if(int(team_2_score) > int(team_1_score)):
+        team_scores[team_1_name] += 0
+        team_scores[team_2_name] += 3
 f.close()
 
+# final result
+sorted_team_scores = [v[0] for v in sorted(
+    team_scores.items(), key=lambda kv: (-kv[1], kv[0]))]
 
-# sorting the teams by value and print alphabetical order
-for team in team_scores:
-    print('team name: ' + team + '. team score: ' + str(team_scores[team]))
+i = 0
+for teams in sorted_team_scores:
+    rank = i + 1
+    print(str(rank) + ". " +
+          sorted_team_scores[i] + " " + str(team_scores[teams]))
+    i += 1
--- showcase.py
+++ showcase.py
@@ -26,17 +26,41 @@ for line in f:
     if (team_scores.get(team_2_name) == None):
         team_scores[team_2_name] = 0
     # assigning points to teams based on win/lose/draw
-    if (team_1_score == team_2_score):
-        team_scores[team_1_name] = team_scores[team_1_name] + 1
-        team_scores[team_1_name] = team_scores[team_2_name] + 1
-    elif(team_1_score > team_2_score):
-        team_scores[team_1_name] = team_scores[team_1_name] + 3
-    elif(team_2_score > team_1_score):
-        team_scores[team_2_name] = team_scores[team_2_name] + 3
+    if (int(team_1_score) == int(team_2_score)):
+        team_scores[team_1_name] += 1
+        team_scores[team_2_name] += 1
+    if(int(team_1_score) > int(team_2_score)):
+        team_scores[team_1_name] += 3
+        team_scores[team_2_name] += 0
+    if(int(team_2_score) > int(team_1_score)):
+        team_scores[team_1_name] += 0
+        team_scores[team_2_name] += 3
 
 f.close()
 
+print("------------------------------------------------------------------------------")
 
-# sorting the teams by value and print alphabetical order
+# displaying team scores unsorted
 for team in team_scores:
     print('team name: ' + team + '. team score: ' + str(team_scores[team]))
+
+print("------------------------------------------------------------------------------")
+
+# sorting based on score
+sorted_team_scores = [v[0] for v in sorted(
+    team_scores.items(), key=lambda kv: (-kv[1], kv[0]))]
+
+i = 0
+for teams in sorted_team_scores:
+    print(sorted_team_scores[i])
+    i += 1
+
+print("------------------------------------------------------------------------------")
+
+# final result
+i = 0
+for teams in sorted_team_scores:
+    rank = i + 1
+    print(str(rank) + ". " +
+          sorted_team_scores[i] + " " + str(team_scores[teams]))
+    i += 1
